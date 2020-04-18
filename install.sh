c1dac60ddef7d91a1ac2d68210377b87517424e3
Jacob Austin <ja3067@columbia.edu>
2020-04-18 16:37:13 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+git clone https://github.com/jacobaustin123/fancy-cd.git ~/Documents/fancy-cd/
 cat >> ~/.bash_profile <<'EOF'
 fancycd() {
     cd "$(python ~/Documents/fancy-cd/fancycd.py "$*")"
